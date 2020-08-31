6a1101c2bb09164927cf746f1b53f95a568a8eef
Abhijnan-Bajpai <abhijnanbajpai@outlook.com>
2020-08-31 22:52:43 +0530

    Basic working model

---
+++ lib/size_helper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+Size displaySize(BuildContext context) {
+  debugPrint('Size = ' + MediaQuery.of(context).size.toString());
+  return MediaQuery.of(context).size;
+}
+
+double displayHeight(BuildContext context) {
+  debugPrint('Height = ' + displaySize(context).height.toString());
+  return displaySize(context).height;
+}
+
+double displayWidth(BuildContext context) {
+  debugPrint('Width = ' + displaySize(context).width.toString());
+  return displaySize(context).width;
+}
